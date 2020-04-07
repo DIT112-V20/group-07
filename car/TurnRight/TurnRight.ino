79b116d20f1060c35622e8176a9ecc672440d220
sijakob <sijakob@student.chalmers.se>
2020-04-07 15:02:22 +0200

    #6 Implement the car turning right
    Implements the car turning to the right with specified amount of degrees.

---
+++ car/TurnRight/TurnRight.ino
@@ -0,0 +1,30 @@
+#include <Smartcar.h>
+
+BrushedMotor leftMotor(smarcarlib::pins::v2::leftMotorPins);
+BrushedMotor rightMotor(smarcarlib::pins::v2::rightMotorPins);
+DifferentialControl control (leftMotor, rightMotor);
+const int TURNING_SPEED = 40;
+
+SimpleCar car(control);
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  goForward(60);
+  delay(1000);
+  turnRight(90);
+}
+
+void turnRight(int angle) {
+  car.setSpeed(TURNING_SPEED);
+  car.setAngle(angle);
+}
+
+void goForward (int speed) {
+  car.setSpeed(speed);
+  car.setAngle(0);
+}
