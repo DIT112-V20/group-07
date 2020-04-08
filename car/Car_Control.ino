51a27c7190bca4cf1ce8f8d2497dc436d87e73cb
hjernos <gushjeros@student.gu.se>
2020-04-08 16:37:45 +0200

    #7 Implemented new logic for stop method

--- car/Car_Control.ino
+++ car/Car_Control.ino
@@ -5,8 +5,6 @@ BrushedMotor rightMotor(smartcarlib::pins::v2::rightMotorPins);
 DifferentialControl control (leftMotor, rightMotor);
 SimpleCar car(control);
 
-const int SPEED = 40; //Speed is 40% of capacity
-
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(9600);
@@ -15,20 +13,12 @@ void setup() {
 void loop() {
   // put your main code here, to run repeatedly:
 
-  delay(500);
-  drive(true);
-  delay(500);
-  drive(false);
+  stop();
 
 }
 
 //Method for stopping the car
-void drive(bool MOVE) {
-   if (MOVE == true) {
-     car.setSpeed(SPEED);
-
-   } else if (MOVE == false) {
-     car.setSpeed(0);
-
-   }
+void stop() {
+   car.setSpeed(0); 
+   
  }
