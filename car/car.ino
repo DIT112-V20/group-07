9448b806587928f963c1620053d2e740cf9a6eba
sijakob <sijakob@student.chalmers.se>
2020-04-10 14:07:43 +0200

    #6 Update turnRight method
    Added the turn right method and made sure to use the offset.

---
+++ car/car.ino
@@ -0,0 +1,34 @@
+
+#include <Smartcar.h>
+
+int trigPin = 19; //D19
+int echoPin = 5; //D5
+int MAX_DISTANCE = 300;
+
+SR04 front(trigPin, echoPin, MAX_DISTANCE);
+BrushedMotor leftMotor(smartcarlib::pins::v2::leftMotorPins);
+BrushedMotor rightMotor(smartcarlib::pins::v2::rightMotorPins);
+DifferentialControl control(leftMotor, rightMotor);
+
+SimpleCar car(control);
+
+void setup()
+{
+
+  Serial.begin(9600);
+  
+}
+
+void loop() {
+  
+  int distance = front.getDistance();
+
+  if (distance <= 15 && distance > 0){ //stop when distance is less than 15 cm.
+  car.setSpeed(0);
+  }
+  else{
+    // Move forward in speed of 40% of the capacaty 
+    car.setSpeed(40);
+  }
+  
+}
