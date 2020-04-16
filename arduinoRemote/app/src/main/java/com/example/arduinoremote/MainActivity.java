5249ed3b183908b5170f3e08bff75bad3f3ed727
sijakob <gussixja@student.gu.se>
2020-04-16 15:14:23 +0200

    #26 Implement forward button

--- arduinoRemote/app/src/main/java/com/example/arduinoremote/MainActivity.java
+++ arduinoRemote/app/src/main/java/com/example/arduinoremote/MainActivity.java
@@ -1,13 +1,42 @@
 package com.example.arduinoremote;
 
+import android.view.View;
+import android.widget.Button;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+//import the bluetooth connectivity thing from the bluetooth group!! (***btSocket*** or whatever name they choose)
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    static char instruction = 's';
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button forwardBut = (Button)findViewById(R.id.forwardBut);
+        Button stopBut = (Button)findViewById(R.id.stopBut);
+        forwardBut.setOnClickListener(this);
+        stopBut.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        //do something when the button is clicked
+        switch (v.getId()) {
+            case R.id.forwardBut:
+                instruction = 'f';
+                btSocket.getOutputStream().write(instruction);
+                break;
+
+            case R.id.stopBut:
+                instruction = 's';
+                btSocket.getOutputStream().write(instruction);
+                break;
+        }
+
+
     }
 }
