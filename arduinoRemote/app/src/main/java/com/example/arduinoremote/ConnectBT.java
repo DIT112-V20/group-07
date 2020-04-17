390c940b3d179d9fb26e382db459157e5b66070c
sijakob <gussixja@student.gu.se>
2020-04-17 11:20:51 +0200

    #26 Implement remotely driving forward
    Implemented driving forward button, stop button and a way to establish bluetooth connection.
    References #27, #23

---
+++ arduinoRemote/app/src/main/java/com/example/arduinoremote/ConnectBT.java
@@ -0,0 +1,37 @@
+package com.example.arduinoremote;
+
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.bluetooth.BluetoothSocket;
+import android.os.AsyncTask;
+import android.widget.Button;
+
+import java.io.IOException;
+import java.util.UUID;
+
+public class ConnectBT extends AsyncTask<Void, Void, Void> {
+    private boolean ConnectSuccess = true; //if it's here, it's almost connected
+    String address = "24:6F:28:B6:09:A2"; //this should be the arduino cars bt address
+    BluetoothAdapter myBluetooth = null;
+    BluetoothSocket btSocket = null;
+    private boolean isBtConnected = false;
+    static final UUID myUUID = UUID.fromString("25dd9b13-b489-4c34-a9b7-31fad9f63b4b");
+
+
+    @Override
+    protected Void doInBackground(Void... devices) { //while the progress dialog is shown, the connection is done in background
+        try {
+            if (btSocket == null || !isBtConnected) {
+                myBluetooth = BluetoothAdapter.getDefaultAdapter();//get the mobile bluetooth device
+                BluetoothDevice enhet = myBluetooth.getRemoteDevice(address);//connects to the device's address and checks if it's available
+                btSocket = enhet.createInsecureRfcommSocketToServiceRecord(myUUID);//create a RFCOMM (SPP) connection
+                BluetoothAdapter.getDefaultAdapter().cancelDiscovery();
+                btSocket.connect();//start connection
+            }
+        }
+        catch (IOException e) {
+            ConnectSuccess = false;//if the try failed, you can check the exception here
+        }
+        return null;
+    }
+}
--- arduinoRemote/app/src/main/java/com/example/arduinoremote/MainActivity.java
+++ arduinoRemote/app/src/main/java/com/example/arduinoremote/MainActivity.java
@@ -1,42 +1,74 @@
 package com.example.arduinoremote;
 
+import android.app.ProgressDialog;
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothSocket;
+import android.content.Intent;
+import android.os.AsyncTask;
 import android.view.View;
 import android.widget.Button;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
-//import the bluetooth connectivity thing from the bluetooth group!! (***btSocket*** or whatever name they choose)
+import java.io.IOException;
+import java.util.UUID;
 
+//import the bluetooth connectivity thing from the bluetooth group!! (***btSocket*** or whatever name they choose)
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
-    static char instruction = 's';
+    static String instruction = "s";
+    static int REQUEST_ENABLE_BT = 1;
+    Button forwardBut, stopBut;
+    String address = "24:6F:28:B6:09:A2"; //this should be the arduino cars bt address
+    BluetoothAdapter myBluetooth = null;
+    BluetoothSocket btSocket = null;
+    private boolean isBtConnected = false;
+    static final UUID myUUID = UUID.fromString("25dd9b13-b489-4c34-a9b7-31fad9f63b4b");
+
+
+    protected void doInBackground(Void... devices) {
+
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Button forwardBut = (Button)findViewById(R.id.forwardBut);
-        Button stopBut = (Button)findViewById(R.id.stopBut);
+        forwardBut = (Button) findViewById(R.id.forwardBut);
+        stopBut = (Button) findViewById(R.id.stopBut);
         forwardBut.setOnClickListener(this);
         stopBut.setOnClickListener(this);
+
+
     }
 
     @Override
     public void onClick(View v) {
         //do something when the button is clicked
-        switch (v.getId()) {
-            case R.id.forwardBut:
-                instruction = 'f';
-                btSocket.getOutputStream().write(instruction);
-                break;
+        if (btSocket != null) {
 
-            case R.id.stopBut:
-                instruction = 's';
-                btSocket.getOutputStream().write(instruction);
-                break;
+
+            switch (v.getId()) {
+                case R.id.forwardBut:
+                    instruction = "f";
+                    try {
+                        btSocket.getOutputStream().write(instruction.getBytes());
+                    } catch (IOException ignored) {
+                    }
+                    break;
+
+                case R.id.stopBut:
+                    instruction = "s";
+                    try {
+                        btSocket.getOutputStream().write(instruction.getBytes());
+                    } catch (IOException ignored) {
+                    }
+                    break;
+            }
         }
 
 
     }
 }
+
