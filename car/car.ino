f3367bf54d355681aa90799017a4f57ff3e55125
hjernos <gushjeros@student.gu.se>
2020-04-17 12:28:35 +0200

    #25 Implement show light when connected
    Method and logic to make the LED-light on the car light-up when the car is connected to bluetooth and blink when the car is disconnected from bluetooth

--- car/car.ino
+++ car/car.ino
@@ -6,7 +6,7 @@ int echoPin = 5; //D5
 int MAX_DISTANCE = 300;
 const auto pulsesPerMeter = 600;
 const int TURN_ANGLE = 80;
-const int REVERS_SPEED = 40; 
+const int REVERS_SPEED = 40;
 
 BrushedMotor leftMotor(smartcarlib::pins::v2::leftMotorPins);
 BrushedMotor rightMotor(smartcarlib::pins::v2::rightMotorPins);
@@ -18,63 +18,70 @@ SR04 front(trigPin, echoPin, MAX_DISTANCE);
 BluetoothSerial SerialBT;//for the BT
 
 DirectionlessOdometer leftOdometer(
-    smartcarlib::pins::v2::leftOdometerPin,
-    []() { leftOdometer.update(); },
-    pulsesPerMeter);
+  smartcarlib::pins::v2::leftOdometerPin,
+[]() {
+  leftOdometer.update();
+},
+pulsesPerMeter);
 DirectionlessOdometer rightOdometer(
-    smartcarlib::pins::v2::rightOdometerPin,
-    []() { rightOdometer.update(); },
-    pulsesPerMeter);
+  smartcarlib::pins::v2::rightOdometerPin,
+[]() {
+  rightOdometer.update();
+},
+pulsesPerMeter);
 
 SmartCar car(control, gyroscope, leftOdometer, rightOdometer);
 
-void callback(esp_spp_cb_event_t event, esp_spp_cb_param_t *param){
-  if(event == ESP_SPP_SRV_OPEN_EVT){
-    Serial.println("Client Connected");
+//Method that detects if the smartcar is connected to bluetooth
+void callback(esp_spp_cb_event_t event, esp_spp_cb_param_t *param) {
+  if (event == ESP_SPP_SRV_OPEN_EVT) { //If the smartcar has a bluetooth connection
+    digitalWrite(LED_BUILTIN, HIGH); // turn the LED on (HIGH is the voltage level)
   }
- 
-  if(event == ESP_SPP_CLOSE_EVT ){
-    Serial.println("Client disconnected");
+
+  if (event == ESP_SPP_CLOSE_EVT ) { //If the smartcar does not have a bluetooth connection
     stop();
+    ledBlink();
   }
 }
 
 void setup() {
-    // put your setup code here, to run once:
+  // put your setup code here, to run once:
   Serial.begin(9600);
   SerialBT.begin("Smartcar"); //Name of the BT in the car
-
+  pinMode(LED_BUILTIN, OUTPUT);
   SerialBT.register_callback(callback);
-   
- }
+
+}
 
 void loop() {
-     // put your main code here, to run repeatedly:
-    handleInput();
+  // put your main code here, to run repeatedly:
+  handleInput();
 }
 
 void handleInput() { //handle serial input if there is any
-       if (SerialBT.available()) {
-           char input;
-            while (SerialBT.available()) { input = SerialBT.read(); }; //read till last character
-            switch (input) {
-                case 'l': //rotate counter-clockwise going forward
-                    turnLeft(TURN_ANGLE);
-                break;
-                case 'r': //turn clock-wise
-                    turnRight(TURN_ANGLE);
-                break;
-                case 'f': //go ahead
-                   driveForward();
-                break;
-                case 'b': //go back
-                    reverse(REVERS_SPEED);
-                break;
-                case 's':
-                    stop();
-                break;
-                default: //if you receive something that you don't know, just stop
-                    stop();
-            }
-       }
+  if (SerialBT.available()) {
+    char input;
+    while (SerialBT.available()) {
+      input = SerialBT.read();
+    }; //read till last character
+    switch (input) {
+      case 'l': //rotate counter-clockwise going forward
+        turnLeft(TURN_ANGLE);
+        break;
+      case 'r': //turn clock-wise
+        turnRight(TURN_ANGLE);
+        break;
+      case 'f': //go ahead
+        driveForward();
+        break;
+      case 'b': //go back
+        reverse(REVERS_SPEED);
+        break;
+      case 's':
+        stop();
+        break;
+      default: //if you receive something that you don't know, just stop
+        stop();
+    }
+  }
 }
