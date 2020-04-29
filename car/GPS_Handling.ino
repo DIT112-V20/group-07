194eaa72c6667357feac5f7bcb0897c46cf5fb6b
sijakob <gussixja@student.gu.se>
2020-04-29 14:48:35 +0200

    #66 Update parsedGPS method
    Added if statement with gps.location.isUpdated()

--- car/GPS_Handling.ino
+++ car/GPS_Handling.ino
@@ -11,9 +11,9 @@ TinyGPSPlus gps;
   while (SerialGPS.available()>0){
     gps.encode(SerialGPS.read());
     delay(2000);
-    //if (gps.location.isUpdated()){ //do we want to update with a delay or with a delay and when the car changes position??
+    if (gps.location.isUpdated()){ //do we want to update with a delay or with a delay and when the car changes position??
       SerialBT.print("Latitude = "); SerialBT.println(gps.location.lat(), 6);  //prints long and lat strings to                                                                          
       SerialBT.print("Longitude = "); SerialBT.println(gps.location.lng(), 6); //bluetooth serial
-    //}
+    }
   }
  }
