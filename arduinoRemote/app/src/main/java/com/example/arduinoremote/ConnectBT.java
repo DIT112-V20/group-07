69d493a31b9ca969aa8ff2eaeb81d1348db826ab
Johann <gustammjo@student.gu.se>
2020-04-30 11:18:38 +0200

    #66 Make the car send a string to the app
    Send a hardcoded string from the car to the app which is displayed in the log of the app
    Add comments to bluetooth input stream

--- arduinoRemote/app/src/main/java/com/example/arduinoremote/ConnectBT.java
+++ arduinoRemote/app/src/main/java/com/example/arduinoremote/ConnectBT.java
@@ -6,6 +6,7 @@ import android.bluetooth.BluetoothSocket;
 
 import java.io.InputStream;
 import java.io.OutputStream;
+
 import android.os.AsyncTask;
 import android.os.Handler;
 import android.util.Log;
@@ -15,6 +16,7 @@ import java.util.Set;
 import java.util.UUID;
 
 public class ConnectBT extends AsyncTask<Void, Void, Void> {
+    final char DELIMITER = '\n';
     String carName = "Smartcar"; //this should be the arduino cars bt address
     BluetoothAdapter myBluetooth = null;
     BluetoothSocket btSocket = null;
@@ -22,79 +24,76 @@ public class ConnectBT extends AsyncTask<Void, Void, Void> {
     OutputStream btOutputStream;
     InputStream btInputStream;
     Boolean stopWorker = false;
-    final char DELIMITER = '\n';
     byte[] readBuffer = new byte[256];
     int readBufferPosition;
-
 
 
     @Override
     protected Void doInBackground(Void... devices) { //while the progress dialog is shown, the connection is done in background
         try {
-            Log.d("Testing","Background");
+            Log.d("Testing", "Background");
 
-                myBluetooth = BluetoothAdapter.getDefaultAdapter();//get the mobile bluetooth device
+            myBluetooth = BluetoothAdapter.getDefaultAdapter();//get the mobile bluetooth device
 
-                Set<BluetoothDevice> pairedDevices = myBluetooth.getBondedDevices();
+            Set<BluetoothDevice> pairedDevices = myBluetooth.getBondedDevices();
 
-                if(pairedDevices.size() > 0) {
+            if (pairedDevices.size() > 0) {
 
-                    for(BluetoothDevice device: pairedDevices) {
+                for (BluetoothDevice device : pairedDevices) {
 
-                        if(device.getName().equals(carName)) {
-                            this.device = device;
-                            UUID uuid = UUID.fromString("00001101-0000-1000-8000-00805f9b34fb"); //Standard SerialPortService ID
-                            btSocket = device.createRfcommSocketToServiceRecord(uuid);
-                            btSocket.connect();
-                            btOutputStream = btSocket.getOutputStream();
-                            btInputStream = btSocket.getInputStream();
+                    if (device.getName().equals(carName)) {
+                        this.device = device;
+                        UUID uuid = UUID.fromString("00001101-0000-1000-8000-00805f9b34fb"); //Standard SerialPortService ID
+                        btSocket = device.createRfcommSocketToServiceRecord(uuid);
+                        btSocket.connect();
+                        btOutputStream = btSocket.getOutputStream();
+                        btInputStream = btSocket.getInputStream();
 
-                            workerThread thread = new workerThread();
-                            new Thread(thread).start();
-                            break;
-                        }
+                        btInThread thread = new btInThread(); // creates a new thread for bluetooth input
+                        new Thread(thread).start(); // starts the thread for bluetooth input
+                        break;
                     }
                 }
-        }
-        catch (IOException ignored) {
+            }
+        } catch (IOException ignored) {
         }
         return null;
     }
 
-        class workerThread implements Runnable{
-            public void run() {
-                while (!Thread.currentThread().isInterrupted() && !stopWorker) {
-                    try{
-                        int bytesAvailable = btInputStream.available();
-                        if (bytesAvailable > 0) {
-                            byte[] packetBytes = new byte[bytesAvailable];
-                            btInputStream.read(packetBytes);
+    // Defines Thread for btInput
+    class btInThread implements Runnable {
+        public void run() {
+            while (!Thread.currentThread().isInterrupted() && !stopWorker) {
+                try {
+                    int bytesAvailable = btInputStream.available(); // Number of availabe bytes
+                    if (bytesAvailable > 0) {
+                        byte[] packetBytes = new byte[bytesAvailable]; // Array for all incoming bytes with the size of available bytes
+                        btInputStream.read(packetBytes); // Copies available bytes from btInputStream to the packetBytes Array
 
-                            for(int i=0;i<bytesAvailable;i++) {
-                                byte b = packetBytes[i];
-                                if(b == DELIMITER)
-                                {
-                                    byte[] encodedBytes = new byte[readBufferPosition];
-                                    System.arraycopy(readBuffer, 0, encodedBytes, 0, encodedBytes.length);
-                                    final String data = new String(encodedBytes, "US-ASCII");
-                                    readBufferPosition = 0;
+                        // goes through the packetBytes Array
+                        for (int i = 0; i < bytesAvailable; i++) {
+                            byte b = packetBytes[i];
+                            if (b == DELIMITER) { //checks if current byte is the end-message symbol (end of one message)
+                                byte[] encodedBytes = new byte[readBufferPosition]; // creates a new Array with the size of the current message
+                                System.arraycopy(readBuffer, 0, encodedBytes, 0, encodedBytes.length); // copies the buffer array to the encoded bytes array
+                                final String data = new String(encodedBytes, "US-ASCII"); // makes a String of the encodedBytes Array
+                                readBufferPosition = 0; // resets the readBuffer pointer
 
-                                    //The variable data now contains our full command
+                                //The variable data now contains our full command
 
-                                    Log.d("Input", data);
-                                }
-                                else
-                                {
-                                    readBuffer[readBufferPosition++] = b;
-                                }
+                                Log.d("Input", data);
+                            } else {
+                                // If the current byte is not the end-message symbol, it places the currenty byte (b) in the readBuffer array and increments the pointer by 1
+                                readBuffer[readBufferPosition++] = b;
                             }
                         }
-
-
-                    } catch (IOException e){
-                        Log.e("Input", e.toString());
                     }
+
+
+                } catch (IOException e) {
+                    Log.e("Input", e.toString());
                 }
             }
         }
+    }
 }
