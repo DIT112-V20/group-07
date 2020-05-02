42b215dd6f94c2ce8f0f922545ddd46d072e733d
Maja <guswennrma@student.gu.se>
2020-05-02 15:59:36 +0200

    #66 Make it possible to visulaize the possition of the car.
    At the moment it shows longitude, latitude, speed and how many satellites it has made contact with, note: the sketch only work on its own atm since it hase a stup and a loop

--- car/GPS_Handling.ino
+++ car/GPS_Handling.ino
@@ -1,19 +1,36 @@
-#include <TinyGPS++.h>
+#include "TinyGPS++.h"
+#include "SoftwareSerial.h"
 
+int TXpin = 17;
+int RXpin = 16;
+
+SoftwareSerial Serial_connect(TXpin, RXpin); 
 TinyGPSPlus gps;
-/*
- *    THIS SHALL BE IN THE CAR FILE
- *
- * SerialGPS.begin(GPSBaud);
- */
 
- void parsedGPS(){
-  while (SerialGPS.available()>0){
-    gps.encode(SerialGPS.read());
-    delay(2000);
-    if (gps.location.isUpdated()){ //do we want to update with a delay or with a delay and when the car changes position??
-      SerialBT.print("Latitude = "); SerialBT.println(gps.location.lat(), 6);  //prints long and lat strings to                                                                          
-      SerialBT.print("Longitude = "); SerialBT.println(gps.location.lng(), 6); //bluetooth serial
-    }
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+  Serial_connect.begin(9600);
+  Serial.println("GPS Start");
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+  while (Serial_connect.available()){
+    gps.encode(Serial_connect.read());  
   }
- }
+
+  if (gps.location.isUpdated()){
+    Serial.println("Satellite Count:");  
+    Serial.println(gps.satellites.value());  
+    Serial.println("Latitude:");  
+    Serial.println(gps.location.lat(), 6);  
+    Serial.println("Longitude:");  
+    Serial.println(gps.location.lng(), 6);  
+    Serial.println("Speed MPS:");
+    Serial.println(gps.speed.mps());
+  }
+
+}
