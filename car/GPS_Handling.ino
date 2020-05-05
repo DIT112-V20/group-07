a09cab971cd4c41f731278afbee2d5793828b603
Johann <gustammjo@student.gu.se>
2020-05-05 21:25:54 +0200

    #66 Update print statements
    Remove newline character from every print for the GPS communication. So that only one \n is send at the end of one message

--- car/GPS_Handling.ino
+++ car/GPS_Handling.ino
@@ -10,15 +10,15 @@ void parsedGPS() {
   
   if (gps.location.isUpdated()){    
     //print to bluetooth
-    SerialBT.println(gps.location.lat(), 6); 
-    SerialBT.println("#");                          //if needed the # is there to split lat and long
-    SerialBT.println(gps.location.lng(), 6 + '\n'); // '\n' is there to know that both lat long has been received
+    SerialBT.print(gps.location.lat(), 6);
+    SerialBT.print("#");                          //if needed the # is there to split lat and long
+    SerialBT.print(gps.location.lng(), 6 + '\n'); // '\n' is there to know that both lat long has been received
 
 
     //test for making sure the gps works using the serial monitor
-   /* Serial.println("Latitude: ");
+   /* Serial.print("Latitude: ");
     Serial.println(gps.location.lat(), 6);
-    Serial.println("Longitude: ");
+    Serial.print("Longitude: ");
     Serial.println(gps.location.lng(), 6);
    */
   }
